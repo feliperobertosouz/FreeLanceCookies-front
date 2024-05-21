a10822569add6059fb2ee123606a8c2fc7bdaff5
Felipe <feliperobertosouz@gmail.com>
2024-05-20 21:49:45 -0300

    feat: added all the pages and html +created the routes to the pages

--- FreelanceCookie/src/app/app.routes.ts
+++ FreelanceCookie/src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { OfertaComponent } from './components/oferta/oferta.component';
 
 export const routes: Routes = [
     {
@@ -8,5 +11,17 @@ export const routes: Routes = [
     {
         path: 'login', 
         component: LoginComponent
+    },
+    {
+        path: 'inicio',
+        component: InicioComponent
+    },
+    {
+        path: 'cadastro',
+        component: CadastroComponent
+    },
+    {
+        path: 'oferta',
+        component: OfertaComponent
     }
 ];
