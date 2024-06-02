4e35cd533b952a7c6a110db9c78e73f3cbb21bdf
Felipe <feliperobertosouz@gmail.com>
2024-06-01 22:37:29 -0300

    feat: added routing

--- FreelanceCookie/src/app/components/login/login.component.ts
+++ FreelanceCookie/src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private http: HttpClient){
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router){
     this.loginForm = this.fb.group({
       email: ['',Validators.required],
       senha: ['',Validators.required]
@@ -29,16 +30,12 @@ export class LoginComponent {
     event.preventDefault();
     console.log('Formulário enviado')
     console.log(this.loginForm.value);
-    // this.http.post('http://localhost:8080/api/auth/login', this.loginObj).subscribe(
-    //   (response) => {
-    //     console.log('Resposta do servidor', response);
-    //   }
-    // )
     this.http.post<any>('http://localhost:8080/api/auth/login', this.loginForm.value).toPromise().then(
       res => {
         console.log('Resposta do servidor', res);
         if(res.message === 'Logado'){
           console.log('Usuário logado');
+          this.router.navigate(['/inicio']);
         }else{
           console.log('Usuário não logado');
         }
