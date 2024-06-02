f21ee850734cd48c057fbded2ed952dcb2ea6e36
Felipe <feliperobertosouz@gmail.com>
2024-06-01 22:20:00 -0300

    feat: made the login logic

--- FreelanceCookie/src/app/components/login/login.component.ts
+++ FreelanceCookie/src/app/components/login/login.component.ts
@@ -1,38 +1,49 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, HttpClientModule],
+  imports: [FormsModule, HttpClientModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  loginObj: Login;
-  constructor (private http: HttpClient, private router: Router) {
-    this.loginObj = new Login();
-    }
-    onLogin() {
-      this.http.post('localhost:3000/login', this.loginObj).subscribe((res: any)=>{
-        if(res.result) {
-          alert("Logado com sucesso");
-          this.router.navigateByUrl('/home');
-        } else{
-          alert(res.message);
-        }
-      })  
-  }
-}
+  loginForm: FormGroup;
 
-export class Login{
-   Email: string;
-   Password: string;
-   constructor(){
-    this.Email = '';
-    this.Password = '';
-   }
-}
+  constructor(private fb: FormBuilder, private http: HttpClient){
+    this.loginForm = this.fb.group({
+      email: ['',Validators.required],
+      senha: ['',Validators.required]
+    })
+  }
+
+  loginObj = {
+    "email": "user@example.com",
+    "senha": "password"
+  }
+
+  onLogin(event: Event){
+    event.preventDefault();
+    console.log('Formulário enviado')
+    console.log(this.loginForm.value);
+    // this.http.post('http://localhost:8080/api/auth/login', this.loginObj).subscribe(
+    //   (response) => {
+    //     console.log('Resposta do servidor', response);
+    //   }
+    // )
+    this.http.post<any>('http://localhost:8080/api/auth/login', this.loginForm.value).toPromise().then(
+      res => {
+        console.log('Resposta do servidor', res);
+        if(res.message === 'Logado'){
+          console.log('Usuário logado');
+        }else{
+          console.log('Usuário não logado');
+        }
+      }
+    );
+    }
+  }
+
